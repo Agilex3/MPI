bbd5ff9cd6e9554079371e715b242659fcd65c75
VulpoiuPetru <petru.vulpoiu@student.unitbv.ro>
2025-03-29 16:49:03 +0200

    Test

--- Catalogue/Program.cs
+++ Catalogue/Program.cs
@@ -38,20 +38,20 @@ using (var scope = app.Services.CreateScope())
         var usersCount = await dbContext.Users.CountAsync();
         Console.WriteLine($"Conexiunea a fost realizat? cu succes! Num?rul de utilizatori: {usersCount}");
 
-        // Adaug? un utilizator de test indiferent de num?rul de utilizatori
-        var user = new User
-        {
-            first_name = "Test",
-            last_name = "User",
-            email = "test.user@example.com",
-            role = "student",
-            created_at = DateTime.Now // Adaug? data cre?rii pentru a respecta structura entit??ii
-        };
-        user.SetPassword("password123"); // Parol? nesigur?, dar f?r? hashing
+        //// Adaug? un utilizator de test indiferent de num?rul de utilizatori
+        //var user = new User
+        //{
+        //    first_name = "Test",
+        //    last_name = "User",
+        //    email = "test.user@example.com",
+        //    role = "student",
+        //    created_at = DateTime.Now // Adaug? data cre?rii pentru a respecta structura entit??ii
+        //};
+        //user.SetPassword("password123"); // Parol? nesigur?, dar f?r? hashing
 
 
-        // Adaug? utilizatorul în baza de date
-        dbContext.Users.Add(user);
+        //// Adaug? utilizatorul în baza de date
+        //dbContext.Users.Add(user);
         try
         {
             // Salveaz? modific?rile în baza de date
