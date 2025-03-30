8096ff8a3ba3177ce15b4dadd0004ef71a3f4bc3
Tomescu Andrei <147052399+Tomescu-Andrei@users.noreply.github.com>
2025-03-30 11:05:09 +0300

    SCRUM-31 Create CourseService

---
+++ Catalogue/Services/CourseService.cs
@@ -0,0 +1,63 @@
+﻿using Catalogue.Data;
+using Catalogue.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Catalogue.Services
+{
+    public class CourseService
+    {
+        private readonly MyDbContext _context;
+
+        public CourseService(MyDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Course>> GetAllCoursesAsync()
+        {
+            return await _context.Courses
+                .Include(c => c.Teacher) // încarcă și profesorul
+                .ToListAsync();
+        }
+
+        public async Task<Course?> GetCourseByIdAsync(int id)
+        {
+            return await _context.Courses
+                .Include(c => c.Teacher)
+                .FirstOrDefaultAsync(c => c.Id == id);
+        }
+
+        public async Task<Course> CreateCourseAsync(Course course)
+        {
+            course.CreatedAt = DateTime.Now;
+            _context.Courses.Add(course);
+            await _context.SaveChangesAsync();
+            return course;
+        }
+
+        public async Task<bool> UpdateCourseAsync(int id, Course updatedCourse)
+        {
+            var existingCourse = await _context.Courses.FindAsync(id);
+            if (existingCourse == null)
+                return false;
+
+            existingCourse.Name = updatedCourse.Name;
+            existingCourse.Description = updatedCourse.Description;
+            existingCourse.TeacherId = updatedCourse.TeacherId;
+
+            await _context.SaveChangesAsync();
+            return true;
+        }
+
+        public async Task<bool> DeleteCourseAsync(int id)
+        {
+            var course = await _context.Courses.FindAsync(id);
+            if (course == null)
+                return false;
+
+            _context.Courses.Remove(course);
+            await _context.SaveChangesAsync();
+            return true;
+        }
+    }
+}
