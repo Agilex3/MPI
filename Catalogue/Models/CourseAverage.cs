9e28ac05900852c6e447d940b434d565cee34d9b
VulpoiuPetru <petru.vulpoiu@student.unitbv.ro>
2025-04-07 00:31:37 +0300

    KEY-52: Create Average Grades

---
+++ Catalogue/Models/CourseAverage.cs
@@ -0,0 +1,8 @@
+﻿namespace Catalogue.Models
+{
+    public class CourseAverage
+    {
+        public string Course { get; set; }
+        public decimal Average { get; set; }
+    }
+}
---
+++ Catalogue/Models/StudentCourseAverage.cs
@@ -0,0 +1,9 @@
+﻿namespace Catalogue.Models
+{
+    public class StudentCourseAverage
+    {
+        public string Student { get; set; }
+        public string Course { get; set; }
+        public decimal Average { get; set; }
+    }
+}
